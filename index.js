9ed9e10c5364d1ef0421bc43ae341b13b5ff7d9e
Royaltea-lang <110403196+Royaltea-lang@users.noreply.github.com>
2022-12-25 20:03:17 +0300

    We created two files.

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+ *Created_by: Robert Musili
+ * Created_date: 25-Dec-2022
+ * About: Entry point to the Todo List
+ * Last Modified_by:
+ * 
+
+*
+* Here, we require express module on node for setting 
+* up the express server
+*/ 
+const express = require('express');
