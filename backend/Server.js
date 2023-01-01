3a45bcad1c016118ed7001b91894e3f389fa7c0c
Royaltea-lang <110403196+Royaltea-lang@users.noreply.github.com>
2023-01-01 14:48:59 +0300

    First code of 2023 on day one of 2023. Able to listen to my Backend server. Able to connect to MongoDB.

---
+++ backend/Server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes/ToDoRoute')
+
+ 
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.port || 5000;
+
+mongoose
+.set("strictQuery", false)
+    .connect(process.env.MONGODB_URL)
+    .then(()=> console.log(`Connected to MongoDB`))
+    .catch((err)=> console.log(err))    
+
+app.use(routes)
+
+app.listen(PORT, ()=> console.log(`Listen on: ${PORT}`));
